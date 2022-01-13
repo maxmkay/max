0d39ac9d70e20ac4ea3ebe357c82cf7035db9735
maxmkay <90427988+maxmkay@users.noreply.github.com>
2022-01-12 23:53:26 -0700

    mission2

---
+++ maxmish2/wwwroot/js/basic.js
@@ -0,0 +1,12 @@
+﻿$("#btnSend").click(function () {
+    answer = (("#assign").val() * .55) + (("#group").val() * .05) + (("#quiz") * .1) + (("#intex") * .2);
+    alert("Your Calculated Grade is: " + answer);
+})
+
+//Assignments 55 %
+//    Group Project 5 %
+//        Quizzes 10 %
+//            Exams 20 %
+&("#btnSend").click(function () {
+    alert("this is working")
+})
